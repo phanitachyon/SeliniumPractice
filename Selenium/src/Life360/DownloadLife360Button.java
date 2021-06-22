629ddfbb784dd2f992cefb0cdf0bd268ec06917d
MohanSai333 <mohan.a@tachyontech.com>
2021-06-22 17:52:18 +0530

    Changed the comments

--- Selenium/src/Life360/DownloadLife360Button.java
+++ Selenium/src/Life360/DownloadLife360Button.java
@@ -21,25 +21,25 @@ public class DownloadLife360Button {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\MohanSai\\Downloads\\chromedriver_win32\\chromedriver.exe");
         ChromeDriver driver=new ChromeDriver();
-        //Navigate to life 360 website
+        // Navigate to life 360 website
         driver.get("https://www.qa.life360.com/intl/");
-        //applying wait
+        // applying wait
         WebDriverWait wait=new WebDriverWait(driver,10);
-        //Click on download
+        // Click on download
         driver.findElement(By.xpath("/html/body/header/div/div/div/a[1]")).click();
-        //Navigate to next tab
+        // Navigate to next tab
         ArrayList<String> newTb = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(newTb.get(1));
-        //Xpath for the required text
+        // Xpath for the required text
          actualText = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/h1")).getText();
          originalText = "Download Life360 for free";
-         //Print output
+         // Print output
         if(actualText.equals(originalText)) {
             System.out.println("Test Passes and same text displayed");
         }else {
             System.out.println("Test case failed same text not displayed");
         }
-        //Close window
+        // Close window
         driver.close();
         }
         }
