17edd591cbf6c8b373eada4097c895ba1ff0c8be
MohanSai333 <mohan.a@tachyontech.com>
2021-06-22 19:36:14 +0530

    Added  new programs

--- Selenium/src/Life360/NavigateToGooglePlayStore.java
+++ Selenium/src/Life360/NavigateToGooglePlayStore.java
@@ -13,19 +13,20 @@ public class NavigateToGooglePlayStore {
         // TODO Auto-generated method stub
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\MohanSai\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        ChromeDriver driver=new ChromeDriver();
+        ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.qa.life360.com/intl/");
-        WebDriverWait wait = new WebDriverWait(driver,50);
+        WebDriverWait wait = new WebDriverWait(driver, 50);
         driver.findElement(By.xpath("/html/body/main/section[1]/div[1]/div/a[2]//img[@class=\"op-download-img googleplay-download\"]")).click();
         ArrayList<String> newTb = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(newTb.get(1));
-        boolean GooglePlayTextIsDisplayed=driver.findElement(By.xpath("//*[@id=\"gbq1\"]/div/a//img[@class=\"gb_uc\"]")).isDisplayed();
-        if(GooglePlayTextIsDisplayed==true) {
+        boolean GooglePlayTextIsDisplayed = driver.findElement(By.xpath("//*[@id=\"gbq1\"]/div/a//img[@class=\"gb_uc\"]")).isDisplayed();
+        if (GooglePlayTextIsDisplayed == true) {
             System.out.println("System is navigated to google play store page");
-        }else {
+        } else {
             System.out.println("System is not navigated to google play store page");
             driver.close();
         }
 
-    }}
+    }
+}
 
