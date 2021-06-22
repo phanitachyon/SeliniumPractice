7456df833c5acb709e078d0876b94566d05e901d
MohanSai333 <mohan.a@tachyontech.com>
2021-06-22 19:22:24 +0530

    Added 2 new programs

--- Selenium/src/Life360/VerifyGooglePlayStoreButtonIsDisplayed.java
+++ Selenium/src/Life360/VerifyGooglePlayStoreButtonIsDisplayed.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class VerifyGooglePlayStoreButtonIsDisplayed {
-    //public static void main(String[] args) throws InterruptedException {
+   
     public void test1() {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\MohanSai\\Downloads\\chromedriver_win32\\chromedriver.exe");
